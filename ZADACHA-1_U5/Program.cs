cad18c229bc266e1a0810a47816299957776d0d0
KostyaVLG <dka1977@yandex.ru>
2023-06-04 14:21:01 +0300

    zdelal_zadachi_5_urok

---
+++ ZADACHA-1_U5/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+CreatedArrayRandomNumbers(numbers);
+Console.WriteLine("В массиве следующие числа: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int j = 0; j < numbers.Length; j++)
+if (numbers[j] % 2 == 0)
+count++;
+
+Console.WriteLine($"Итого всего в массиве {numbers.Length} чисел, а {count} их них чётные");
+
+void CreatedArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(2,10000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ ZADACHA-2_U5/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+CreatedArrayRandomNumbers(numbers);
+Console.WriteLine("В массиве следующие числа: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int j = 0; j < numbers.Length; j+=2)
+    sum = sum + numbers[j];
+
+    Console.WriteLine($"Всего {numbers.Length} чисел");
+    Console.WriteLine($"Сумма чисел на нечётных позициях = {sum}");
+void CreatedArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10000);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
